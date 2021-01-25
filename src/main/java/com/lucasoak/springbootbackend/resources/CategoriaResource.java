ac9e5e13fed321eefafb1e7017b7b7f458759348
devLucasOAK <devlucasoak@outook.com>
2021-01-25 16:32:40 -0300

    rest working

---
+++ src/main/java/com/lucasoak/springbootbackend/resources/CategoriaResource.java
@@ -0,0 +1,15 @@
+package com.lucasoak.springbootbackend.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaResource {
+	
+	@RequestMapping(method =  RequestMethod.GET)
+	private String listar() {
+		return "Rest working";
+	}
+}
