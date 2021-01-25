619f358003c2ec1b50e4ecb454fa42f4c4590864
devLucasOAK <devlucasoak@outook.com>
2021-01-25 16:47:35 -0300

    Testando a primeira classe domain (Categoria)

--- src/main/java/com/lucasoak/springbootbackend/resources/CategoriaResource.java
+++ src/main/java/com/lucasoak/springbootbackend/resources/CategoriaResource.java
@@ -1,15 +1,28 @@
 package com.lucasoak.springbootbackend.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.lucasoak.springbootbackend.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method =  RequestMethod.GET)
-	private String listar() {
-		return "Rest working";
+	private List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informatica");
+		Categoria cat2 = new Categoria(2, "Escritorio");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 }
