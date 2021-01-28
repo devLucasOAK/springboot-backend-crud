afa033dd7f806aabadcfabd46ca24a434588362d
devLucasOAK <devlucasoak@outook.com>
2021-01-27 23:02:05 -0300

    operacao de instanciacao com CommandLineRunner

--- src/main/java/com/lucasoak/springbootbackend/SpringbootBackendApplication.java
+++ src/main/java/com/lucasoak/springbootbackend/SpringbootBackendApplication.java
@@ -1,13 +1,34 @@
 package com.lucasoak.springbootbackend;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.lucasoak.springbootbackend.domain.Categoria;
+import com.lucasoak.springbootbackend.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class SpringbootBackendApplication {
+public class SpringbootBackendApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootBackendApplication.class, args);
     }
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
+	}
 
 }
