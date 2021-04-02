8140c68faf7062cf05f6cd1698ded106e0732068
devLucasOAK <devlucasoak@outook.com>
2021-04-02 14:01:21 -0300

    Exception Handlers

--- src/main/java/com/lucasoak/springbootbackend/services/CategoriaService.java
+++ src/main/java/com/lucasoak/springbootbackend/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.lucasoak.springbootbackend.domain.Categoria;
 import com.lucasoak.springbootbackend.repositories.CategoriaRepository;
+import com.lucasoak.springbootbackend.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -17,6 +18,7 @@ public class CategoriaService {
 	public Categoria buscar(Integer id) {
 		
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 }
