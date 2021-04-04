457636083a9fd612741ecd66183a0c4011aa0ac7
devLucasOAK <devlucasoak@outook.com>
2021-04-04 00:29:24 -0300

    Put Categoria, translate methods

--- src/main/java/com/lucasoak/springbootbackend/resources/ClienteResource.java
+++ src/main/java/com/lucasoak/springbootbackend/resources/ClienteResource.java
@@ -18,9 +18,9 @@ public class ClienteResource {
 	private ClienteService service;
 	
 	@RequestMapping(value="/{id}", method =  RequestMethod.GET)
-	private ResponseEntity<?> find(@PathVariable Integer id) {
+	private ResponseEntity<Cliente> find(@PathVariable Integer id) {
 		
-		Cliente obj = service.buscar(id);
+		Cliente obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
 		
 	}
--- src/main/java/com/lucasoak/springbootbackend/resources/PedidoResource.java
+++ src/main/java/com/lucasoak/springbootbackend/resources/PedidoResource.java
@@ -18,9 +18,9 @@ public class PedidoResource {
 	private PedidoService service;
 	
 	@RequestMapping(value="/{id}", method =  RequestMethod.GET)
-	private ResponseEntity<?> find(@PathVariable Integer id) {
+	private ResponseEntity<Pedido> find(@PathVariable Integer id) {
 		
-		Pedido obj = service.buscar(id);
+		Pedido obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
 		
 	}
