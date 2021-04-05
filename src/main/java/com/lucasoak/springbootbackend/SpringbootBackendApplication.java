1f3a518c0b232ef3c8052b550297769e43650924
devLucasOAK <devlucasoak@outook.com>
2021-04-04 22:17:22 -0300

    Paginação Categoria

--- src/main/java/com/lucasoak/springbootbackend/SpringbootBackendApplication.java
+++ src/main/java/com/lucasoak/springbootbackend/SpringbootBackendApplication.java
@@ -63,6 +63,10 @@ public class SpringbootBackendApplication implements CommandLineRunner{
 		
 		Categoria cat1 = new Categoria(null, "Informatica");
 		Categoria cat2 = new Categoria(null, "Escritorio");
+		Categoria cat3 = new Categoria(null, "Eletronicos");
+		Categoria cat4 = new Categoria(null, "Jardinagem");
+		Categoria cat5 = new Categoria(null, "Casa Mesa e Banho");
+		Categoria cat6 = new Categoria(null, "Gamer");
 		
 		Produto p1 = new Produto(null, "computador", 2000.00);
 		Produto p2 = new Produto(null, "Impressora", 800.00);
@@ -75,7 +79,7 @@ public class SpringbootBackendApplication implements CommandLineRunner{
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
-		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 		
 		Estado est1 = new Estado(null, "Minas Gerais");
